8eb81709caff276e3d22560da48a73418ad8bff0
thisisyomans <tanejamm@gmail.com>
2019-05-21 14:16:54 -0700

    new test

--- maintext.py
+++ maintext.py
@@ -1,10 +1,10 @@
 import textrank
 
 textrank.setup_environment()
-filename = 'tests/bmw/bmw.txt'
+filename = 'tests/ari/editorial.txt'
 with open(filename) as f:
 	summary = textrank.extract_sentences(f.read(), 108)
 	print(summary)
-	text_file = open("tests/bmw/Output.txt", "w")
+	text_file = open("tests/ari/Output.txt", "w")
 	text_file.write(summary)
 	text_file.close()
